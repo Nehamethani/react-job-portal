19c53a13bfdc57a79034a91203dfa9b1f1c438b6
Neha Methani <nehamethani54@gmail.com>
2024-10-30 00:49:48 +0530

    Updated Router and added pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Hero from "../Components/Hero";
 import HeroCards from "../Components/HeroCards";
 import JobListings from "../Components/JobListings";
+import ViewAllJobs from "../Components/ViewAllJobs"
 const HomePage = () => {
   return (
     <>
       <Hero/>
       <HeroCards/>
-      <JobListings/>
+      <JobListings isHome={true}/>
+      <ViewAllJobs/>
     </>
   )
 }
