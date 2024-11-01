62fde21009013fa38b09cba4ee5567a8b2392e72
Neha Methani <nehamethani54@gmail.com>
2024-11-01 19:03:44 +0530

    Jobpage Component

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   server :  {
     proxy: {
       '/api' : {
-        target: 'http://localhost:8000/jobs',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
-    }
+    } 
   }
 })
