6954d4827b27ca4aa9be5805d662efda88accb33
Neha Methani <nehamethani54@gmail.com>
2024-11-01 23:23:32 +0530

    POST/DELETE jobs call added

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,46 @@ import React from 'react';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
-import AddJobs from './pages/AddJobs';
+import AddJobPage from './pages/AddJobPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout/>}>
-  <Route index element= {<HomePage/>}/>
-  <Route path='/jobs' element= {<JobsPage/>}/>
-  <Route path='/add-job' element= {<AddJobs/>}/>
-  <Route path='*' element= {<NotFoundPage/>}/>
-  <Route path='/jobs/:id' element= {<JobPage/>}/>
-  </Route>
-  
-  )
-);
+import JobPage, {jobLoader} from './pages/JobPage';
+
 
 const App = () => {
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers : {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+    
+  };
+
+  //Delete a Job
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+    <Route index element= {<HomePage/>}/>
+    <Route path='/jobs' element= {<JobsPage/>}/>
+    <Route path='/add-job' element= {<AddJobPage addJobSubmit={addJob}/>}/>
+    <Route path='*' element= {<NotFoundPage/>}/>
+    <Route path='/jobs/:id' element= {<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
+    </Route>
+    
+    )
+  );
   return <RouterProvider router={router} />;
 }
 
